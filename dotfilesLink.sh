df5781ea782418f2fab3e29869935e4743650d58
daichidd <dddpga1@gmail.com>
2018-04-01 02:10:33 +0900

    add symbolic link sh

---
+++ dotfilesLink.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+ln -sf ~/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/dotfiles/.vim ~/.vim
+ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
+ln -sf ~/dotfiles/.profile ~/.profile
+ln -sf ~/dotdiles/.bashrc ~/.bashrc
